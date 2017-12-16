6040bcc8e416fd79fe2fa0a967196acd0e12ecdd
leepuppychow <leepuppychow@gmail.com>
2017-12-16 12:09:21 -0700

    Laying out welcome page, wait for authentication/authorization lectures

---
+++ spec/features/user_can_see_homepage_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe "When anyone (visitor, logged-in user, admin) visits homepage path" do
+  it "sees a Welcome page with form to login or create new user" do
+    visit root_path
+
+    save_and_open_page
+
+    expect(page).to have_content("Health Tracker - Parkinson's")
+    expect(page).to have_content "Username"
+    expect(page).to have_content "Password"
+    expect(page).to have_content "Login"
+    expect(page).to have_content "Sign up as new user"
+    expect(page).to have_content "Visit the site"
+  end
+end
