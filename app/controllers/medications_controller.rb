6fd0bc2938d4b0ea1d7b96bcdaf951e6dff1d42a
leepuppychow <leepuppychow@gmail.com>
2017-12-16 20:43:55 -0700

    Able to add new medication with nested form_for helper

--- app/controllers/medications_controller.rb
+++ app/controllers/medications_controller.rb
@@ -7,17 +7,30 @@ class MedicationsController < ApplicationController
   def new
     @patient = Patient.find(params[:patient_id])
     @medication = Medication.new
-
   end
 
   def create
-    medication = Medication.new(medication_params)
+    patient = Patient.find(params[:patient_id])
+    medication = Medication.create(medication_params)
+    new_med = patient.patient_medications.new(dosage_frequency_params)
+    new_med.medication_id = medication.id
+    if medication.save
+      flash[:success] = "Added new medication successfully!"
+      redirect_to patient_medications_path(patient)
+    else
+      flash[:error] = "Please enter all information"
+      redirect_to new_patient_medication_path(patient)
+    end
   end
 
   private
 
     def medication_params
       params.require(:medication).permit(:name)
+    end
+
+    def dosage_frequency_params
+      params[:medication].require(:medications).permit(:dosage, :freq_per_day)
     end
 
 end
--- app/models/medication.rb
+++ app/models/medication.rb
@@ -1,6 +1,8 @@
 class Medication < ApplicationRecord
   validates :name, presence: true
   has_many :patient_medications
-  has_many :patients, through: :patient_medications 
+  has_many :patients, through: :patient_medications
+
+  accepts_nested_attributes_for :patient_medications
 
 end
