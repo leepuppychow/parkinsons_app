68ba631fcc88e1152f426f5343b9b5c423e49f79
leepuppychow <leepuppychow@gmail.com>
2017-12-18 00:15:44 -0700

    Edit therapist functionality working

--- app/controllers/therapists_controller.rb
+++ app/controllers/therapists_controller.rb
@@ -6,4 +6,56 @@ class TherapistsController < ApplicationController
     @doctor = @patient.doctor
   end
 
+  def new
+    @patient = Patient.find(params[:patient_id])
+    @therapist = @patient.therapists.new
+  end
+
+  def create
+    # patient = Patient.find(params[:patient_id])
+    # therapist = patient.therapists.create!(therapist_params)
+    # redirect_to patient_therapists_path(patient)
+
+    # therapist = patient.therapists.new(therapist_params)
+    # if therapist.save
+    #   redirect_to patient_therapists_path(patient)
+    # else
+    #   redirect_to new_patient_therapist_path(patient)
+    # end
+
+    patient = Patient.find(params[:patient_id])
+    if therapist_params.values.include?("")
+      redirect_to new_patient_therapist_path(patient)
+    else
+      patient.therapists.create!(therapist_params)
+      redirect_to patient_therapists_path(patient)
+    end
+  end
+
+  def edit
+    @patient = Patient.find(params[:patient_id])
+    @therapist = Therapist.find(params[:id])
+  end
+
+  def update
+    patient = Patient.find(params[:patient_id])
+    therapist = Therapist.find(params[:id])
+    if therapist.update(therapist_params)
+      therapist.update(therapist_params)
+      redirect_to patient_therapists_path(patient)
+    else
+      redirect_to edit_patient_therapist_path(patient, therapist)
+    end 
+  end
+
+  private
+
+    def therapist_params
+      params.require(:therapist).permit(:first_name,
+                                        :last_name,
+                                        :years_experience,
+                                        :therapy_type,
+                                        :patient)
+    end
+
 end
--- spec/features/therapists/new_spec.rb
+++ spec/features/therapists/new_spec.rb
@@ -3,6 +3,9 @@ require 'rails_helper'
 describe "When user visits My Care Team" do
   it "can click on New Therapist and create new doctor with a form" do
     @patient = create(:patient)
+    create(:therapist, therapy_type: "Physical Therapy")
+    create(:therapist, therapy_type: "Occupational Therapy")
+    create(:therapist, therapy_type: "Speech Therapy")
 
     visit patient_therapists_path(@patient)
 
@@ -10,18 +13,16 @@ describe "When user visits My Care Team" do
 
     expect(current_path).to eq new_patient_therapist_path(@patient)
     expect(page).to have_content "Add New Therapist"
-    expect(page).to have_content "First Name"
-    expect(page).to have_content "Last Name"
-    expect(page).to have_content "Type of Therapy"
+    expect(page).to have_content "First name"
+    expect(page).to have_content "Last name"
+    expect(page).to have_content "Therapy type"
     expect(page).to have_content "Experience"
+
 
     fill_in "therapist[first_name]", with: "Taylor"
     fill_in "therapist[last_name]", with: "McCulloch"
-    select "Physical Therapy", :from => "therapist[type_of_therapy]"
+    select "Physical Therapy", :from => "therapist[therapy_type]"
     fill_in "therapist[years_experience]", with: 2
-
-    # <th><%= f.label :category_id, class: "form-label" %></th>
-    # <th><%= f.collection_select :category_id, Category.all, :id, :title, {}, id: "drop-down-menu" %></th>
 
     click_on "Add!"
 
