71a000b0295842dee5ad9373cf68951f39f38563
leepuppychow <leepuppychow@gmail.com>
2017-12-19 11:31:23 -0700

    Added authorization to admin patient index page, visitors cannot access

--- app/controllers/admin/patients_controller.rb
+++ app/controllers/admin/patients_controller.rb
@@ -1,7 +1,7 @@
-class Admin::PatientsController < ApplicationController
+class Admin::PatientsController < Admin::BaseController
 
   def index
-    @patients = Patient.all 
+    @patients = Patient.all
   end
 
 end
--- spec/features/admin_can_see_patients(users)_index_spec.rb
+++ spec/features/admin_can_see_patients(users)_index_spec.rb
@@ -4,7 +4,7 @@ describe "A user visits Patient index page" do
   context "as an ADMIN" do
     it "allows admin to see all the patients" do
       create_list(:patient, 10)
-      
+
       admin = create(:patient, username: "admin", password: "password", role: 1)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
@@ -12,6 +12,20 @@ describe "A user visits Patient index page" do
       visit admin_patients_path
       expect(page).to have_content("Admin")
       expect(page).to have_content("All current patients")
+    end
+  end
+
+  context "as a Visitor or Patient(logged-in user)" do
+    it "cannot see the admin patients index" do
+      user = create(:patient, role: 0)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit admin_patients_path
+
+      expect(page).to_not have_content("Admin")
+      expect(page).to_not have_content("All current patients")
+      expect(page).to have_content "The page you were looking for doesn't exist"
     end
   end
 end
