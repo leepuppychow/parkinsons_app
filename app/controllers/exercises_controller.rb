d46efca0f2f880dd3ad688eea4554d3688fba38e
leepuppychow <leepuppychow@gmail.com>
2017-12-18 21:49:32 -0700

    Working on adding exercise to patient_exercises

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -8,4 +8,11 @@ class ExercisesController < ApplicationController
     @my_exercises = @patient.patient_exercises
   end
 
+  def update
+    @patient = Patient.find(params[:patient_id])
+    @exercise = Exercise.find(params[:id])
+    byebug
+  end
+
+
 end
