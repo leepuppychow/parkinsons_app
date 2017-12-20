a5b266d46b003725c8cadcc0fd043015349facc2
leepuppychow <leepuppychow@gmail.com>
2017-12-20 14:37:21 -0700

    Add new activity test passing

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -10,7 +10,20 @@ class ActivitiesController < ApplicationController
   end
 
   def create
-    byebug
+    @patient = Patient.find(params[:patient_id])
+    activity = @patient.activities.new(activity_params)
+    if activity.save
+      flash[:success] = "Logged new activity: #{activity.description}"
+      redirect_to patient_activities_path(@patient)
+    else
+      render :new
+    end
   end
+
+  private
+
+    def activity_params
+      params.require(:activity).permit(:description, :duration, :date_performed)
+    end
 
 end
--- spec/features/activities/patient_can_add_new_activity_spec.rb
+++ spec/features/activities/patient_can_add_new_activity_spec.rb
@@ -1,25 +1,24 @@
 require 'rails_helper'
 
-describe "When a patient visits their homepage" do
-  context "he/she can click on Activity Tracker" do
-    it "can click on Log Activity link and be sent to a form to add a new activity" do
-      patient = create(:patient)
-      create_list(:exercise, 10)
+describe "When a patient visits activity index" do
+  it "can click on Log Activity link and be sent to a form to add a new activity" do
+    patient = create(:patient)
 
-      visit patient_activities_path(patient)
+    visit patient_activities_path(patient)
 
-      click_on "Log Activity"
+    click_on "Log New Activity"
 
-      expect(current_path).to eq new_patient_activity_path(patient)
-      expect(page).to have_content "Log Activity Levels"
-      expect(page).to have_content "Total(min)"
-      expect(page).to have_content "Date"
+    expect(current_path).to eq new_patient_activity_path(patient)
+    expect(page).to have_content "Log Activity Levels"
+    expect(page).to have_content "Total(min)"
+    expect(page).to have_content "Date"
 
-      fill_in "activity[duration]", with: "20"
-      fill_in "activity[date_performed]", with: Date.today
-      fill_in "activity[description]", with: "Did today's workout"
+    fill_in "activity[description]", with: "Did today's workout"
+    fill_in "activity[duration]", with: "20"
+    fill_in "activity[date_performed]", with: Date.today
 
-      click_on "Log Activity"
+    click_on "Log Activity"
 
-      expect(current_path).to eq patient_exercises_path(patient)
-    end
+    expect(current_path).to eq patient_activities_path(patient)
+  end
+end
