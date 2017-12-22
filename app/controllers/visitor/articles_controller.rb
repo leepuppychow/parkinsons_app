6f7e82745652b6831a4aab83ae58e1b340108b54
leepuppychow <leepuppychow@gmail.com>
2017-12-22 02:08:28 -0700

    Pubmed article search working with titles showing

--- app/controllers/visitor/articles_controller.rb
+++ app/controllers/visitor/articles_controller.rb
@@ -11,15 +11,29 @@ class Visitor::ArticlesController < ApplicationController
 
     response = Faraday.get("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term=#{@search}+AND+#{@year}[pdat]")
     raw_data = response.body
-
     unprocessed_ids = Nokogiri::XML(raw_data).xpath("//Id").to_s
     pubmed_ids = unprocessed_ids.delete('<Id>').split("/")
 
-    pubmed_ids.each do |id|
-      @articles << Article.new(title: ".", content: ".", address: id)
-    end
+    summaries = Faraday.get("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&id=#{pubmed_ids.join(",")}")
+    summaries_body = Nokogiri::XML(summaries.body)
+    titles = summaries_body.xpath('//*[@Name="Title"]').map {|element| element.children.text}
+    @articles = pubmed_ids.zip(titles) if pubmed_ids
 
     render :index
+#THIS API CAN TAKE THE IDs and GET DOCUMENT SUMMARY!
+# ESummary (document summary downloads)
+# eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi
+# Responds to a list of UIDs from a given database with the corresponding document summaries.
+
+# Basic Downloading
+# esummary.fcgi?db=<database>&id=<uid_list>
+# Input: List of UIDs (&id); Entrez database (&db)
+#
+# Output: XML DocSums
+#
+# Example: Download DocSums for these protein GIs: 6678417,9507199,28558982,28558984,28558988,28558990
+#
+# https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=protein&id=6678417,9507199,28558982,28558984,28558988,28558990
   end
 
 end
