183fb5be150cd3474869db2532669e75a394b829
leepuppychow <leepuppychow@gmail.com>
2017-12-22 14:27:17 -0700

    Doctor delete feature test passing

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -7,15 +7,21 @@ class DoctorsController < ApplicationController
 
   def create
     @patient = Patient.find(params[:patient_id])
-    if @patient.doctor
-      @patient.doctor.update(doctor_params)
+    @doctor = @patient.doctors.find_or_create_by(doctor_params)
+    if @doctor.id == nil
+      render :new
     else
-      doctor = Doctor.create!(doctor_params)
-#there was an issue with patient not saving due to not having password
-#using update_attribute allowed you to update and save the doctor, without needing all attributes
-      @patient.update_attribute(:doctor, doctor)
+      redirect_to patient_therapists_path(@patient)
     end
-    redirect_to patient_therapists_path(@patient)
+#     if @patient.doctor
+#       @patient.doctor.update(doctor_params)
+#     else
+#       doctor = Doctor.create!(doctor_params)
+# #there was an issue with patient not saving due to not having password
+# #using update_attribute allowed you to update and save the doctor, without needing all attributes
+#       @patient.update_attribute(:doctor, doctor)
+#     end
+#     redirect_to patient_therapists_path(@patient)
   end
 
   def edit
@@ -35,7 +41,6 @@ class DoctorsController < ApplicationController
 
   def destroy
     patient = Patient.find(params[:patient_id])
-#delete the patient_doctor instead of the doctor object
     doctor = patient.patient_doctors.find_by(doctor_id: params[:id])
     doctor.destroy
     redirect_to patient_therapists_path(patient)
