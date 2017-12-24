f0dcd93a5ba51b9988b4eaf7b86fa29f4539f82a
leepuppychow <leepuppychow@gmail.com>
2017-12-23 17:42:13 -0700

    Sorted activities by date

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,7 +2,7 @@ class ActivitiesController < ApplicationController
 
   def index
     @patient = Patient.find(params[:patient_id])
-    @activities = @patient.activities
+    @activities = @patient.activities.order("date_performed DESC")
   end
 
   def new
