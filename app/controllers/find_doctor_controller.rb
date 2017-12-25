e152eda58cdda12108a45870ff0278ac9750ce5e
leepuppychow <leepuppychow@gmail.com>
2017-12-24 17:40:03 -0700

    Refactoring find_doctor_controller

--- app/controllers/find_doctor_controller.rb
+++ app/controllers/find_doctor_controller.rb
@@ -4,6 +4,22 @@ class FindDoctorController < ApplicationController
   def index
     @patient = Patient.find(params[:patient_id])
     @doctors = []
+    @specialty = ""
+  end
+
+  def doctor_name(practice)
+    practice["practices"].first["name"]
+  end
+
+  def full_address(practice)
+    practice["practices"].first["visit_address"]["street"] + " " +
+    practice["practices"].first["visit_address"]["city"] + ", " +
+    practice["practices"].first["visit_address"]["state"] + " " +
+    practice["practices"].first["visit_address"]["zip"]
+  end
+
+  def phone_number(practice)
+    practice["practices"].first["phones"].first["number"]
   end
 
   def create
@@ -11,29 +27,18 @@ class FindDoctorController < ApplicationController
     city = params[:city].strip.downcase
     state = params[:state].strip.downcase
     @specialty = params[:specialty].strip.downcase
+    @doctors = []
 
   #34f9ff67082e6e43cfb2dc335e45decf
     response = Faraday.get("https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=#{@specialty}&location=#{state}-#{city}&limit=20&user_key=34f9ff67082e6e43cfb2dc335e45decf")
-
     data = JSON.parse(response.body)
 
-    @doctors = []
-
     if data["data"]
-      data["data"].each do |practice|
-        @doctors <<
-          [practice["practices"].first["name"],
-
-          practice["practices"].first["visit_address"]["street"] + " " +
-          practice["practices"].first["visit_address"]["city"] + ", " +
-          practice["practices"].first["visit_address"]["state"] + " " +
-          practice["practices"].first["visit_address"]["zip"],
-
-          practice["practices"].first["phones"].first["number"]]
-      end
-      @doctors.uniq!
+      @doctors = data["data"].map do |practice|
+        [doctor_name(practice), full_address(practice), phone_number(practice)]
+      end.uniq
     end
-
+  
     render :index
   end
 
