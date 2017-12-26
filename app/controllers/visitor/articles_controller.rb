b29eba2a7b5df586c9706e8d3c747dd7f6248e25
leepuppychow <leepuppychow@gmail.com>
2017-12-25 20:48:19 -0700

    Removed comments from articles_controller (was saving for blog post)

--- app/controllers/visitor/articles_controller.rb
+++ app/controllers/visitor/articles_controller.rb
@@ -20,20 +20,6 @@ class Visitor::ArticlesController < ApplicationController
     @articles = pubmed_ids.zip(titles) if pubmed_ids
 
     render :index
-#THIS API CAN TAKE THE IDs and GET DOCUMENT SUMMARY!
-# ESummary (document summary downloads)
-# eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi
-# Responds to a list of UIDs from a given database with the corresponding document summaries.
-
-# Basic Downloading
-# esummary.fcgi?db=<database>&id=<uid_list>
-# Input: List of UIDs (&id); Entrez database (&db)
-#
-# Output: XML DocSums
-#
-# Example: Download DocSums for these protein GIs: 6678417,9507199,28558982,28558984,28558988,28558990
-#
-# https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=protein&id=6678417,9507199,28558982,28558984,28558988,28558990
   end
 
 end
