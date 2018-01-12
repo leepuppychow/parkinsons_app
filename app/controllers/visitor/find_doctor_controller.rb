d139ea645c2aeae89ada79e3df86a6b6f93b5c82
leepuppychow <leepuppychow@gmail.com>
2018-01-12 10:17:14 -0700

    Visitor find doctor feature working

--- app/controllers/visitor/find_doctor_controller.rb
+++ app/controllers/visitor/find_doctor_controller.rb
@@ -4,55 +4,55 @@ class Visitor::FindDoctorController < ApplicationController
   include AuthenticationKeys
   before_action :specialties, :states
 
-  def index
-    @doctors = []
-    @specialty = ""
-    @city = ""
-    @state = ""
-  end
-
-  def doctor_name(practice)
-    practice["practices"].first["name"]
-  end
-
-  def full_address(practice)
-    practice["practices"].first["visit_address"]["street"] + " " +
-    practice["practices"].first["visit_address"]["city"] + ", " +
-    practice["practices"].first["visit_address"]["state"] + " " +
-    practice["practices"].first["visit_address"]["zip"]
-  end
-
-  def phone_number(practice)
-    practice["practices"].first["phones"].first["number"]
-  end
-
-  def city_formatted(city)
-    city.strip.downcase.tr(" ", "-")
-  end
-
-  def insurances_accepted(practice)
-    practice["practices"].first["insurance_uids"]
-  end
-
-  def create
-    @city = city_formatted(params[:city])
-    @state = params[:state].strip.downcase
-    @specialty = params[:specialty].strip.downcase
-    @doctors = []
-
-    response = Faraday.get("https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=#{@specialty}&location=#{@state}-#{@city}&limit=20&user_key=#{BETTER_DOCTOR_KEY}")
-    data = JSON.parse(response.body)
-
-    if data["data"]
-      @doctors = data["data"].map do |practice|
-        [doctor_name(practice),
-        full_address(practice),
-        phone_number(practice),
-        insurances_accepted(practice)]
-      end.uniq
+    def index
+      @doctors = Hash.new()
+      @specialty = ""
+      @city = ""
+      @state = ""
     end
 
-    render :index
-  end
+    def doctor_name(practice)
+      practice["practices"].first["name"]
+    end
 
+    def full_address(practice)
+      practice["practices"].first["visit_address"]["street"] + " " +
+      practice["practices"].first["visit_address"]["city"] + ", " +
+      practice["practices"].first["visit_address"]["state"] + " " +
+      practice["practices"].first["visit_address"]["zip"]
+    end
+
+    def phone_number(practice)
+      practice["practices"].first["phones"].first["number"]
+    end
+
+    def insurances_accepted(practice)
+      practice["practices"].first["insurance_uids"]
+    end
+
+    def city_formatted(city)
+      city.strip.downcase.tr(" ", "-")
+    end
+
+    def create
+      @city = city_formatted(params[:city])
+      @state = params[:state].strip.downcase
+      @specialty = params[:specialty].strip.downcase
+      @doctors = Hash.new()
+
+      response = Faraday.get("https://api.betterdoctor.com/2016-03-01/doctors?specialty_uid=#{@specialty}&location=#{@state}-#{@city}&limit=20&user_key=#{BETTER_DOCTOR_KEY}")
+      data = JSON.parse(response.body)
+
+      if data["data"]
+        data["data"].each do |practice|
+          @doctors["#{doctor_name(practice)}"] = {
+            :specialty => @specialty,
+            :location => full_address(practice),
+            :phone_number => phone_number(practice),
+            :insurance => insurances_accepted(practice)
+          }
+        end
+      end
+      render :index
+    end
 end
