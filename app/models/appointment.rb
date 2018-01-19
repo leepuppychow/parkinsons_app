1c4d97803af8ba0144aecdc3a8274fe84a335673
leepuppychow <leepuppychow@gmail.com>
2018-01-18 19:51:56 -0700

    Added scope to Appointment model for future and past appointments

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,4 +2,7 @@ class Appointment < ApplicationRecord
   validates :date_time, presence: true
   belongs_to :patient
   belongs_to :appointable, polymorphic: true
+  scope :future, -> {where("date_time >= '#{Date.today}'")}
+  scope :past, -> {where("date_time < '#{Date.today}'")}
+
 end
