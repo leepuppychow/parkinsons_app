a1ff4b14377f34dd1c82a97978956c1e217d94c8
leepuppychow <leepuppychow@gmail.com>
2018-02-03 15:36:46 -0700

    Added VCR gem for testing BetterDoctor API

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,13 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/cassettes"
+  config.hook_into :webmock
+  config.filter_sensitive_data("better_doctor_key") {ENV["BETTER_DOCTOR_KEY"]}
+end
+
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
