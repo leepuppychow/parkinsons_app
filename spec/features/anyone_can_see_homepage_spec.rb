99f02cf1b625e2f8e9da95e2b17a4b5f8bf5a6e9
leepuppychow <leepuppychow@gmail.com>
2018-02-20 16:00:57 -0700

    anyone can see homepage spec test passing

--- spec/features/anyone_can_see_homepage_spec.rb
+++ spec/features/anyone_can_see_homepage_spec.rb
@@ -5,11 +5,9 @@ describe "When anyone (visitor, logged-in user, admin) visits homepage path" do
     visit root_path
 
     expect(page).to have_content "Health Tracker (Parkinson's)"
-    expect(page).to have_content "New user?"
-    expect(page).to have_content "Sign Up"
-    expect(page).to have_content "Already have an account?"
-    expect(page).to have_content "Log In"
-    expect(page).to have_content "Check out the app!"
-    expect(page).to have_content "Visitor"
+    expect(page).to have_content "Login with Google"
+    expect(page).to have_content "Login with Facebook"
+    expect(page).to have_content "Login"
+    expect(page).to have_content "Create New Account"
   end
 end
