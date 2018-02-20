2199ad298f49a0bc4a691c88c0f5c35cfc883a7a
leepuppychow <leepuppychow@gmail.com>
2018-02-20 16:15:50 -0700

    Delete doctor spec test passing

--- spec/features/doctors/delete_spec.rb
+++ spec/features/doctors/delete_spec.rb
@@ -4,6 +4,7 @@ describe "When user visits My Care Team" do
   it "can click on Remove to delete doctor" do
     patient = create(:patient, role: 2)
     doctor = create(:doctor)
+    doctor.note = Note.create(contents: "", noteable_id: doctor.id, noteable_type: doctor.class.name)
     patient.doctors << doctor
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(patient)
 
