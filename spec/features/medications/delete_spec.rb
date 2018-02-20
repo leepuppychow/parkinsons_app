ea2d97836aa06355791295581838d0ae26a839fd
leepuppychow <leepuppychow@gmail.com>
2018-02-20 16:28:35 -0700

    Delete medication test passing (needed to create a note)'

--- spec/features/medications/delete_spec.rb
+++ spec/features/medications/delete_spec.rb
@@ -4,7 +4,10 @@ describe "When user visits My Medications" do
   it "can click on Remove to remove a medication" do
     patient = create(:patient)
     medication = create(:medication)
-    patient.patient_medications << create(:patient_medication, medication: medication)
+    pm1 = create(:patient_medication, medication: medication)
+    pm1.note = Note.create(contents: "", noteable_id: pm1.id, noteable_type: pm1.class.name)
+    patient.patient_medications << pm1
+
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(patient)
 
     visit patient_medications_path(patient)
