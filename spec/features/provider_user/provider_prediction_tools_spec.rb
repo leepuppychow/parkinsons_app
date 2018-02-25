c35df4c28a725e32e907860724ad34f044fa7c99
leepuppychow <leepuppychow@gmail.com>
2018-02-24 18:55:06 -0700

    Wrote feature test for Doctor Prediction Page

---
+++ spec/features/provider_user/provider_prediction_tools_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe "As a Doctor/Provider user" do
+  context "can access a Prediction Tool page" do
+    it "that can predict patient's fall risk based on age, berg balance, and gait speed" do
+      user = create(:user)
+      doctor = create(:doctor)
+      user.doctor = doctor
+
+      visit doctor_tools_path(doctor)
+
+      fill_in "age", with: 75
+      fill_in "berg", with: 35    # do raw Berg score (out of 56 total)
+      fill_in "gait", with: 0.6   # meters/second
+      click_on "Predict Fall Risk"
+
+      expect(current_path).to eq doctor_tools_path(doctor)
+      expect(page).to have_content "High Fall Risk"
+      expect(page).to have_content "Model Accuracy: 80%"
+    end
+  end
+end
