3a52f953f3f104bb9d24506d0969122dc687cd15
leepuppychow <leepuppychow@gmail.com>
2018-02-28 13:08:23 -0700

    Doctor search API will now search by name and specialty

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::DoctorsController < ApiController
 
   def index
-    render json: Doctor.where("name LIKE ? OR specialty LIKE ?", "%#{params[:name]}%", "%#{params[:name]}%")
+    render json: Doctor.where("name LIKE ? OR specialty LIKE ?",
+              "%#{params[:name]}%", "%#{params[:name]}%")
   end
 
 end
